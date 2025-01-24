4969e574763bb57e7fb683d89c17d245b9821c25
vrushabh <vru@.com>
2025-01-24 13:09:51 +0530

    aap

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
-import os
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from jenkins</h1>'
-@app.route('/hi')
-def hell():
-    return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
+def home():
+    app.logger.info(f"Request received from {request.remote_addr}")
+    return "Python App Hosting"
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=5000, debug=True)
