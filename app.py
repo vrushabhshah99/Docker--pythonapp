a9b18cbe8bf2b7affc91ee4907345cd6490fd27e
vrushabh <vru@.com>
2025-01-27 12:21:12 +0530

    finally

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     app.logger.info(f"Request received from {request.remote_addr}")
-    return "I Am Hosting My App"
+    return "Hi I Am Hosting My App"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
