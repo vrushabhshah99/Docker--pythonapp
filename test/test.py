0dff4c783df3f0e772e2a1153534acbb7bbaed33
vrushabh <vru@.com>
2025-01-27 12:26:56 +0530

    finally

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         tester = app.test_client()
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Python App Hosting")
+        self.assertEqual(response.data, b"Hi I Am Hosting My App")
 
 
 if __name__ == "__main__":
